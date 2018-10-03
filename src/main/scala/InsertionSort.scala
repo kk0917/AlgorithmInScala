32f26d8eef07f6b00f1d2fc6fd4368bc580f3202
ko-kamenashi <ko-kamenashi@dac.co.jp>
2018-10-03 09:13:45 +0900

    first commit

---
+++ src/main/scala/InsertionSort.scala
@@ -0,0 +1,17 @@
+import scala.collection.mutable.ListBuffer
+
+object InsertionSort extends App {
+  val list = ListBuffer(3, 1, 5, 4, 2)
+  for (i <- 0 until list.length) {
+    val v = list(i)
+    var j = i - 1
+    println(j)
+
+    while (j >= 0 && list(j) > v) {
+      list(j + 1) = list(j)
+      j -= 1
+    }
+    list(j + 1) = v
+    println(list)
+  }
+}
