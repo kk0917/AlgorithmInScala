0696ddebd9c4c26be1c3cc71de5aa08f95366c55
Ko Kamenashi <kamenashi0917@gmail.com>
2018-11-09 08:53:21 +0900

    Bubble Sortの修正とビルドファイルの更新

--- src/main/scala/BubbleSort.scala
+++ src/main/scala/BubbleSort.scala
@@ -4,7 +4,9 @@ object BubbleSort extends App {
   var list      = ListBuffer(3, 1, 5, 4, 2)
   var listCount = 1
   while (listCount < list.length) {
-    for (i <- (list.length - listCount) to 0) {
+    for (i <- (list.length - 1) to 1 by -1) {
+      println(list(i))
+      println(list(i - 1))
       if (list(i) < list(i - 1)) {
         val tmp = list(i - 1)
         list(i - 1) = list(i)
